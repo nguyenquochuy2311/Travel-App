ab0177ca97b15d99b5ffc905ecb95e6aa6b15d0c
huycafe24vn <huy@cafe24corp.vn>
2022-06-17 09:28:46 +0700

    done api

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,3 +1,4 @@
+import authRouter from './authRouter.js';
 import usersRouter from './usersRouter.js';
 import permissionsRouter from './permissionsRouter.js';
 import uploadFileRouter from './uploadFileRouter.js';
@@ -8,9 +9,14 @@ import serviceRouter from './serviceRouter.js';
 import refBookingOutcomeRouter from './refBookingOutcomeRouter.js';
 import refBookingStatusPaymentRouter from './refBookingStatusPayment.js';
 import bookingRouter from './bookingRouter.js';
+import restaurantsRouter from './restaurantsRouter.js';
+import airlinesRouter from './airlinesRouter.js';
+import carsRouter from './carsRouter.js';
+import hotelsRouter from './hotelsRouter.js';
+import paymentsRouter from './paymentsRouter.js';
 
 let apiRouter = (app) => {
-    // app.use('/api/v1/auth', authRouter);
+    app.use('/api/v1/auth', authRouter);
 
     app.use('/api/v1/users', usersRouter);
 
@@ -24,6 +30,14 @@ let apiRouter = (app) => {
 
     app.use('/api/v1/country', countryRouter);
 
+    app.use('/api/v1/restaurants', restaurantsRouter);
+
+    app.use('/api/v1/airlines', airlinesRouter);
+
+    app.use('/api/v1/hotels', hotelsRouter);
+
+    app.use('/api/v1/cars', carsRouter);
+
     app.use('/api/v1/services', serviceRouter);
 
     app.use('/api/v1/outcome', refBookingOutcomeRouter);
@@ -31,6 +45,8 @@ let apiRouter = (app) => {
     app.use('/api/v1/statuspayment', refBookingStatusPaymentRouter);
 
     app.use('/api/v1/booking', bookingRouter);
+
+    app.use('/api/v1/payments', paymentsRouter);
 }
 
 module.exports = apiRouter;
